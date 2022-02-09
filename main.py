c26e493e1b0912fe04624af06abb6b8d7c697d2d
A1bertoVG <82616218+A1bertoVG@users.noreply.github.com>
2022-02-09 11:07:27 -0600

    Main code

---
+++ main.py
@@ -0,0 +1,14 @@
+#Main code
+
+# import time
+# from turtle import delay
+# import RPi.GPIO as GPIO
+# import os, cmd
+# from tracemalloc import start
+import rasp_motors
+import Schallsensor
+
+def selfpark():
+    rasp_motors.dir_foward()
+
+
